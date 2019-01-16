3914e801fd82e58d6a9371e373d475864c62bb2f
fjguipen <fjguipen@gmail.com>
2019-01-16 16:31:38 +0100

    + Comentarios

--- assets/js/Calculadora.js
+++ assets/js/Calculadora.js
@@ -2,10 +2,18 @@
  * Clase calculadora: Es el core del modulo calculadora. Utliza la clase Display para manejar
  * la información de salida
  *
- * @param state Objeto que
+ * @param state Objeto que controla el estado actual de la calculadora
+ *      data: Array donde almaceno los valores para la operación y el display:
+ *          POS1(PrimerOperando), POS2(Signo), POS3(SegundoOperando)
+ *      pos: Puntero para determinar en qué posicion se insertó la ultima vez
+ *      powered: Controla si la calculadora está encendida o no
+ *      err: Controla si ha surgido algun error en la operación
+ * @param display Objeto del tipo Display utilizado para mostrar la información de saluida
+ *
  */
 class Calculadora {
     constructor() {
+        //Inicializacion de la calculadora apagada
         this.state = {
             data: ["", "", ""],
             pos: 0,
@@ -31,11 +39,23 @@ class Calculadora {
         this.state.pos = 0;
         this.display.updateDisplay(this.state.data[0]);
     }
+    /**
+     * Funcion para controlar qué ocurre cuando se pulsa cualquier tecla,
+     * (excepto reset() y power() que tienen funcionamientos propios) de forma
+     * que se controla si se ha pulsado un número o un signo, el punto..etc.
+     *
+     * @param value Valor de la tecla pulsada
+     * @param type Tipo de tecla pulsada
+     */
     onKeyPress(value, type) {
+        /*Si la calculadora está apagada o se produjo error en la operación,
+        no se hace nada.*/
         if (!this.state.powered || this.state.err) {
             return;
         }
         switch (type) {
+            /*Cuando se hace click en un numero, se tiene en cuenta si
+            es parte del primer operando o el segundo*/
             case "number":
                 if (this.state.pos === -1) {
                     this.state.data[0] = "";
--- assets/js/Calculadora.ts
+++ assets/js/Calculadora.ts
@@ -1,8 +1,15 @@
 /**
  * Clase calculadora: Es el core del modulo calculadora. Utliza la clase Display para manejar
  * la información de salida
- * 
- * @param state Objeto que 
+ *  
+ * @param state Objeto que controla el estado actual de la calculadora
+ *      data: Array donde almaceno los valores para la operación y el display: 
+ *          POS1(PrimerOperando), POS2(Signo), POS3(SegundoOperando)
+ *      pos: Puntero para determinar en qué posicion se insertó la ultima vez
+ *      powered: Controla si la calculadora está encendida o no
+ *      err: Controla si ha surgido algun error en la operación
+ * @param display Objeto del tipo Display utilizado para mostrar la información de saluida
+ *
  */
 
 class Calculadora {
@@ -15,6 +22,7 @@ class Calculadora {
     display: Display;
 
     constructor() {
+        //Inicializacion de la calculadora apagada
         this.state = {
             data: ["", "", ""],
             pos: 0,
@@ -23,6 +31,7 @@ class Calculadora {
         }
         this.display = new Display();
     }
+
     power(): void {
         if (this.state.powered) {
             this.turnOff();
@@ -39,10 +48,23 @@ class Calculadora {
         this.state.pos = 0;
         this.display.updateDisplay(this.state.data[0]);
     }
-
+    /**
+     * Funcion para controlar qué ocurre cuando se pulsa cualquier tecla, 
+     * (excepto reset() y power() que tienen funcionamientos propios) de forma
+     * que se controla si se ha pulsado un número o un signo, el punto..etc.
+     * 
+     * @param value Valor de la tecla pulsada
+     * @param type Tipo de tecla pulsada
+     */
     onKeyPress(value, type): void {
+
+        /*Si la calculadora está apagada o se produjo error en la operación,
+        no se hace nada.*/
         if (!this.state.powered || this.state.err) { return }
+        
         switch (type) {
+            /*Cuando se hace click en un numero, se tiene en cuenta si 
+            es parte del primer operando o el segundo*/
             case "number":
                 if (this.state.pos === -1) {
                     this.state.data[0] = "";
@@ -57,6 +79,7 @@ class Calculadora {
                     }
                     this.state.data[this.state.pos] = "";
                 }
+        
                 this.state.data[this.state.pos] += value;
                 this.display.updateDisplay(this.state.data[this.state.pos].toString())
 
